86281cd4a2c184b8c264777964a8e50ffd3244dc
PEM-- <pemarchandet@gmail.com>
2015-09-07 09:21:17 +0200

    Footer theming

--- app/packages/footer/footer.jsx
+++ app/packages/footer/footer.jsx
@@ -1,8 +1,23 @@
 Footer = React.createClass({
   render() {
+    console.log('Footer', Meteor.settings.public);
     return (
       <footer>
-        <p>Quelques liens</p>
+        <div className='ui container'>
+          <section>
+            <ul>
+              <li>Liens mentions légales</li>
+              <li>Liens confidentialité</li>
+            </ul>
+          </section>
+          <section>
+            <ul>
+              <li>Twitter</li>
+              <li>Facebook</li>
+              <li>Mail</li>
+            </ul>
+          </section>
+        </div>
       </footer>
     );
   }
