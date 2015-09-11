5f2b7ebb839e0bf3baf229bbf3877f4601b5b270
PEM-- <pemarchandet@gmail.com>
2015-09-11 16:28:48 +0200

    Basic pages - pending

--- app/packages/routing/routing.js
+++ app/packages/routing/routing.js
@@ -4,6 +4,4 @@ console.log('Setting SSR cache');
 const TIME_IN_MS = 1000 * 10;
 FlowRouter.setPageCacheTimeout(TIME_IN_MS);
 // Defer Script loading
-// FlowRouter.setDeferScriptLoading(true);
-
-// @TODO Create the notFoundRoute : https://github.com/kadirahq/flow-router#not-found-routes
+FlowRouter.setDeferScriptLoading(true);
