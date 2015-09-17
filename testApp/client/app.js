4f7f5e49117e33b3eb03ee5f4a60f12acaf50ea2
PEM-- <pemarchandet@gmail.com>
2015-09-17 15:58:59 +0200

    Popup menu pending

--- testApp/client/app.js
+++ testApp/client/app.js
@@ -11,3 +11,16 @@ FlowRouter.route('/menu', {
     BlazeLayout.render('popupMenu');
   }
 });
+
+// Template.popupMenu.onRendered(function() {
+//   $('.hamburger.right.aligned.column h1').popup({
+//     inline: true,
+//     lastResort: true,
+//     hoverable: true,
+//     position: 'bottom left',
+//     // delay: {
+//     //   show: 300,
+//     //   hide: 800
+//     // }
+//   });
+// });
