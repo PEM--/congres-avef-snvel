3d1c24a8422e6cac1502e5ae9cc446e2ee356fff
PEM-- <pemarchandet@gmail.com>
2015-09-24 11:34:08 +0200

    Breaking Menu in smaller components

--- app/packages/client-menu/client-menu.jsx
+++ app/packages/client-menu/client-menu.jsx
@@ -3,9 +3,71 @@
 // Namespace flatteinng
 const { PropTypes, Component } = React;
 
+// Create a logger
+const log = Logger.createLogger('Client Menu');
+
+// Popup menu
+//class PopupMenu extends Rc.MeteorReactBaseComponent {
+class PopupMenu extends Component {
+  displayName: 'PopupMenu'
+  constructor(props) {
+    super(props);
+  }
+  // Render the component
+  render() {
+    log.debug('Rendering PopupMenu');
+    return (
+      <aside className={`client ui vertical menu ${this.props.menuState ? 'open': ''}`}>
+        <i className="fa fa-close close fa-2x"></i>
+        <a href="#" className="item">
+          <i className="fa fa-user"></i>
+          <span className="readability-black">Connexion</span>
+        </a>
+        <a href="#" className="item">
+          <i className="fa fa-unlock"></i>
+          <span className="readability-black">Déconnexion</span>
+        </a>
+        <a href="#" className="active item">
+          <i className="fa fa-home"></i>
+          <span className="readability-black">Accueil</span>
+        </a>
+        <a href="#" className="item">
+          <i className="fa fa-bullhorn"></i>
+          <span className="readability-black">Présentation</span>
+        </a>
+        <a href="#" className="item">
+          <i className="fa fa-calendar"></i>
+          <span className="readability-black">Programme</span>
+        </a>
+        <a href="#" className="item">
+          <i className="fa fa-ticket"></i>
+          <span className="readability-black">Inscriptions</span>
+        </a>
+        <a href="#" className="item">
+          <i className="fa fa-thumbs-o-up"></i>
+          <span className="readability-black">Partenaires &amp; exposants</span>
+        </a>
+      </aside>
+    );
+  }
+}
+PopupMenu.defaultProps = { menuState: false };
+PopupMenu.propTypes = { menuState: React.PropTypes.bool.isRequired };
+
 // Menu component
-class Menu extends Component {
-  displayName: 'Menu'
+//class Menu extends Rc.MeteorReactBaseComponent {
+const Menu = React.createClass({
+  displayName: 'Menu',
+  getInitialState() {
+    return {
+      popupMenuToggle: false
+    };
+  },
+  menuToggle(e) {
+    e.preventDefault();
+    log.debug('Toggling PopupMenu');
+    this.setState({popupMenuToggle: true});
+  },
   // Render the component
   render() {
     log.debug('Rendering Menu');
@@ -20,45 +82,14 @@ class Menu extends Component {
               </h1>
             </div>
             <div className="brand seven wide tablet only seven wide computer only column"><a href="http://www.avef.fr" target="_blank"><img src="/img/avef.svg" alt="AVEF" className="svg"/></a><a href="http://www.snvel.fr" target="_blank"><img src="/img/snvel.svg" alt="SNVEL" className="svg"/></a></div>
-            <div className="hamburger right aligned two wide mobile one wide tablet one wide computer column"><i className="fa fa-bars"></i>
-              <aside className="client ui vertical menu">
-                <i className="fa fa-close close fa-2x"></i>
-                <a href="#" className="item">
-                  <i className="fa fa-user"></i>
-                  <span className="readability-black">Connexion</span>
-                </a>
-                <a href="#" className="item">
-                  <i className="fa fa-unlock"></i>
-                  <span className="readability-black">Déconnexion</span>
-                </a>
-                <a href="#" className="active item">
-                  <i className="fa fa-home"></i>
-                  <span className="readability-black">Accueil</span>
-                </a>
-                <a href="#" className="item">
-                  <i className="fa fa-bullhorn"></i>
-                  <span className="readability-black">Présentation</span>
-                </a>
-                <a href="#" className="item">
-                  <i className="fa fa-calendar"></i>
-                  <span className="readability-black">Programme</span>
-                </a>
-                <a href="#" className="item">
-                  <i className="fa fa-ticket"></i>
-                  <span className="readability-black">Inscriptions</span>
-                </a>
-                <a href="#" className="item">
-                  <i className="fa fa-thumbs-o-up"></i>
-                  <span className="readability-black">Partenaires &amp; exposants</span>
-                </a>
-              </aside>
-            </div>
+            <button onClick={this.menuToggle} className="hamburger right aligned two wide mobile one wide tablet one wide computer column"><i className="fa fa-bars"></i></button>
+            <PopupMenu menuState={this.state.popupMenuToggle}/>
           </div>
         </div>
       </nav>
     );
   }
-}
+});
 
 // Export class
 Rc.Client.Menu = Menu;
