926d2262a12c2101432325be0b2a2144ac6c82df
PEM-- <pemarchandet@gmail.com>
2015-09-24 21:47:36 +0200

    Loader

--- app/packages/client-spinkit/client-spinkit.jsx
+++ app/packages/client-spinkit/client-spinkit.jsx
@@ -8,11 +8,17 @@ const Spinkit = React.createClass({
   displayName: 'Spinkit',
   render() {
     log.debug('Rendering');
+    // let nodes = [];
+    // for (let idx = 0; idx < 9; idx++) {
+    //   nodes.push(<div />);
+    // }
+    // return (<div className="spinner spinner-cube-grid">{nodes}</div>);
+    //return (<div className="spinner spinner-double-bounce"></div>);
     let nodes = [];
-    for (let idx = 0; idx < 9; idx++) {
+    for (let idx = 0; idx < 3; idx++) {
       nodes.push(<div />);
     }
-    return (<div className="spinner spinner-cube-grid">{nodes}</div>);
+    return (<div className="spinner spinner-three-bounce">{nodes}</div>);
   }
 });
 
