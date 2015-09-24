0cb978cdf73a25705f7a54f22c18c2d0a30917aa
PEM-- <pemarchandet@gmail.com>
2015-09-24 15:46:34 +0200

    Default basic pages

--- app/packages/custom-basicpages/cookie.js
+++ app/packages/custom-basicpages/cookie.js
@@ -1,48 +1,104 @@
 DEFAULT_COOKIE = `
 ## 1. Qui sommes-nous ?
-Bienvenue sur le site https://congres-avef-snvel.fr édité par l'AVEF et le SNVEL (consultez nos mentions légales).
+Bienvenue sur le site https://congres-avef-snvel.fr édité par l'[AVEF](http://www.avef.fr)
+([consulteznos mentions légales](/legal)).
 
-Cette rubrique est consacrée à notre politique de gestion des cookies. Elle vous permet d'en savoir plus sur l'origine et l'usage des informations de navigation que les cookies peuvent enregistrer sur votre navigateur.
-
-Cette politique est donc importante pour vous, qui souhaitez avoir une expérience positive et confiante de nos services et pour nous, qui souhaitons répondre de manière précise et complète à vos questions sur l'utilisation que nous faisons des cookies et tenir compte de vos souhaits.
-
-Lors de l'affichage de nos contenus, des informations relatives à la navigation de votre terminal (ordinateur, tablette, smartphone, etc.) sont susceptibles d'être enregistrées dans des fichiers Cookies installés sur votre terminal, sous réserve des choix que vous avez exprimés concernant les cookies et que vous pouvez modifier à tout moment (Cf. section 4).
+Cette rubrique est consacrée à notre politique de gestion des cookies. Elle vous
+permet d'en savoir plus sur l'origine et l'usage des informations de navigation
+que les cookies peuvent enregistrer sur votre navigateur.Cette politique est donc
+importante pour vous, qui souhaitez avoir une expérience positive et confiante
+de nos services et pour nous, qui souhaitons répondre de manière précise et
+complète à vos questions sur l'utilisation que nous faisons des cookies et tenir
+compte de vos souhaits.Lors de l'affichage de nos contenus, des informations
+relatives à la navigation de votre terminal (ordinateur, tablette, smartphone, etc.)
+sont susceptibles d'être enregistrées dans des fichiers Cookies installés sur
+votre terminal, sous réserve des choix que vous avez exprimés concernant les
+cookies et que vous pouvez modifier à tout moment (Cf. section 4).
 
 ## 2. Qu'est ce qu'un cookie et à quoi sert-il ?
-Un cookie (ou témoin de connexion) est un fichier texte susceptible d'être enregistré, sous réserve de vos choix, dans un espace dédié du disque dur de votre terminal (ordinateur, tablette, smartphone, etc.) à l'occasion de la consultation d'un service en ligne grâce à votre logiciel de navigation. Il est transmis par le serveur d'un site internet à votre navigateur. A chaque cookie est attribué un identifiant anonyme. Le fichier cookie permet à son émetteur d'identifier le terminal dans lequel il est enregistré pendant la durée de validité ou d'enregistrement du cookie concerné. Un cookie ne permet pas de remonter à une personne physique.
+Un cookie (ou témoin de connexion) est un fichier texte susceptible d'être
+enregistré, sous réserve de vos choix, dans un espace dédié du disque dur de
+votre terminal (ordinateur, tablette, smartphone, etc.) à l'occasion de la
+consultation d'un service en ligne grâce à votre logiciel de navigation. Il est
+transmis par le serveur d'un site internet à votre navigateur. A chaque cookie
+est attribué un identifiant anonyme. Le fichier cookie permet à son émetteur
+d'identifier le terminal dans lequel il est enregistré pendant la durée de
+validité ou d'enregistrement du cookie concerné. Un cookie ne permet pas de
+remonter à une personne physique.Lorsque vous consultez le site
+https://congres-avef-snvel.fr nous pouvons être amenés à installer, sous réserve
+de votre choix, différents cookies : des cookies de performance.
 
-Lorsque vous consultez le site https://congres-avef-snvel.fr nous pouvons être amenés à installer, sous réserve de votre choix, différents cookies : des cookies de performance.
+## 3. Type de cookie utilisésLes Cookies que nous émettons sur notre site sont
+d'une unique catégorie : la performance. Nous enregistrons des métriques nous
+permettant de vérifier la performance de nos services. Ces enregistrements sont
+parfaitement anonymes et s'appuient sur les meilleures pratiques de
+confidentialité en vigueur. Aucune information relative à un individu ne peut
+en être extrait.
 
-## 3. Type de cookie utilisés
-Les Cookies que nous émettons sur notre site sont d'une unique catégorie : la performance. Nous enregistrons des métriques nous permettant de vérifier la performance de nos services. Ces enregistrements sont parfaitement anonymes et s'appuient sur les meilleures pratiques de confidentialité en vigueur. Aucune information relative à un individu ne peut en être extrait.
+Ce site utilise [Google Analytics](http://www.google.com/intl/fr_fr/analytics/),
+un service d'analyse de site internet fourni par [Google Inc](http://www.google.com/intl/fr_fr/about/company/).
+[Google Analytics](http://www.google.com/intl/fr_fr/analytics/) utilise des
+cookies pour nous aider à analyser l'utilisation du site par ses visiteurs.
+Les données générées par les cookies concernant votre utilisation du site
+(y compris votre adresse IP) seront transmises et stockées par [Google Inc](http://www.google.com/intl/fr_fr/about/company/)
+ sur des serveurs situés aux Etats- Unis. Nous utilisons cette information dans
+ le but d'évaluer votre utilisation du site, de compiler des rapports sur
+ l'activité du site et de fournir d'autres services relatifs à l'activité du
+ site et à l'utilisation d'Internet. Vous pouvez obtenir plus d'informations
+ sur le service [Google Analytics](http://www.google.com/intl/fr_fr/analytics/)
+ en consultant la page suivante : http://www.google.com/analytics/learn/privacy.html
 
-Ce site utilise Google Analytics, un service d'analyse de site internet fourni par Google Inc. Google Analytics utilise des cookies pour nous aider à analyser l'utilisation du site par ses visiteurs. Les données générées par les cookies concernant votre utilisation du site (y compris votre adresse IP) seront transmises et stockées par Google sur des serveurs situés aux Etats- Unis. Nous utilisons cette information dans le but d'évaluer votre utilisation du site, de compiler des rapports sur l'activité du site et de fournir d'autres services relatifs à l'activité du site et à l'utilisation d'Internet. Vous pouvez obtenir plus d'informations sur le service Google Analytics en consultant la page suivante : http://www.google.com/analytics/learn/privacy.html
+ ## 4. Comment gérer vos cookies sur ce site
+ Vous pouvez paramétrer les cookies que nous utilisons via les modalités offertes
+ par votre logiciel de navigation :Vous pouvez ainsi configurer votre logiciel
+ de navigation de manière à ce que des cookies soient enregistrés dans votre
+ terminal ou, au contraire, qu'ils soient rejetés, soit systématiquement, soit
+ selon leur émetteur. Vous pouvez également configurer votre logiciel de
+ navigation de manière à ce que l'acceptation ou le refus des cookies vous soient
+ proposés ponctuellement, avant qu'un cookie soit susceptible d'être enregistré
+ dans votre terminal.
 
-## 4. Comment gérer vos cookies sur ce site
-Vous pouvez paramétrer les cookies que nous utilisons via les modalités offertes par votre logiciel de navigation :
+ La gestion des cookies et de vos choix avec cette option est différente pour
+ chacun d'entre eux. Elle est décrite dans le menu d'aide de votre navigateur,
+ qui vous permettra de savoir de quelle manière modifier vos souhaits en matière
+ de cookies.
 
-Vous pouvez ainsi configurer votre logiciel de navigation de manière à ce que des cookies soient enregistrés dans votre terminal ou, au contraire, qu'ils soient rejetés, soit systématiquement, soit selon leur émetteur. Vous pouvez également configurer votre logiciel de navigation de manière à ce que l'acceptation ou le refus des cookies vous soient proposés ponctuellement, avant qu'un cookie soit susceptible d'être enregistré dans votre terminal.
+Nous mettons à votre disposition les liens des pages d'informations vous permettant
+de configurer vous-même votre navigateur :
 
-La gestion des cookies et de vos choix avec cette option est différente pour chacun d'entre eux. Elle est décrite dans le menu d'aide de votre navigateur, qui vous permettra de savoir de quelle manière modifier vos souhaits en matière de cookies.
-
-Nous mettons à votre disposition les liens des pages d'informations vous permettant de configurer vous-même votre navigateur :
-
-* Internet ExplorerTM : http://windows.microsoft.com/fr-FR/windows-vista/
-* Block-or-allow-cookies SafariTM : http://docs.info.apple.com/
+* Internet ExplorerTM : http://windows.microsoft.com/fr-FR/windows-vista/Block-or-allow-cookies
+* SafariTM : http://docs.info.apple.com/
 * ChromeTM : http://support.google.com/chrome/
 * firefoxTM : http://support.mozilla.org/fr/
 * OperaTM : http://help.opera.com/Windows/10.20/fr/cookies.html
 
-Une fois vos préférences sauvegardées, nous n'utiliserons en lecture ou en écriture que les cookies autorisés par vos règles. Tout cookie enregistré avant votre paramétrage sera toujours sur votre ordinateur, et vous pourrez également le supprimer dans les réglages de votre navigateur.
+Une fois vos préférences sauvegardées, nous n'utiliserons en lecture ou en écriture
+que les cookies autorisés par vos règles. Tout cookie enregistré avant votre
+paramétrage sera toujours sur votre ordinateur, et vous pourrez également le
+supprimer dans les réglages de votre navigateur.
 
 ## 5. Ce qu'implique le refus des cookies
-Si vous refusez l'enregistrement de cookies dans votre terminal, ou si vous supprimez ceux qui y sont enregistrés, vous ne pourrez plus bénéficier d'un certain nombre de fonctionnalités qui sont néanmoins nécessaires pour l'utilisation de nos services.
+Si vous refusez l'enregistrement de cookies dans votre terminal, ou si vous
+supprimez ceux qui y sont enregistrés, vous ne pourrez plus bénéficier d'un
+certain nombre de fonctionnalités qui sont néanmoins nécessaires pour l'utilisation
+de nos services.Tel serait le cas si vous tentiez d'accéder à nos services qu
+nécessitent de vous reconnaitre. Tel serait également le cas lorsque nous ou
+nos prestataires ne pourrions pas définir, à des fins de compatibilité technique,
+le type de navigateur utilisé par votre terminal, ses paramètres de langue et
+d'affichage ou le pays depuis lequel votre terminal semble connecté à Internet.
 
-Tel serait le cas si vous tentiez d'accéder à nos services qui nécessitent de vous reconnaitre. Tel serait également le cas lorsque nous ou nos prestataires ne pourrions pas définir, à des fins de compatibilité technique, le type de navigateur utilisé par votre terminal, ses paramètres de langue et d'affichage ou le pays depuis lequel votre terminal semble connecté à Internet.
-
-Le cas échéant, le site https://congres-avef-snvel.fr décline toute responsabilité concernant les conséquences liées au fonctionnement dégradé de nos services résultant de l'impossibilité d'enregistrer ou de consulter les cookies nécessaires au fonctionnement du site et que vous auriez refusés ou supprimés.
+Le cas échéant, le site https://congres-avef-snvel.fr décline toute responsabilité
+concernant les conséquences liées au fonctionnement dégradé de nos services
+résultant de l'impossibilité d'enregistrer ou de consulter les cookies nécessaires
+au fonctionnement du site et que vous auriez refusés ou supprimés.
 
 ## 6. Si vous partagez l'utilisation de votre terminal avec d'autres personnes
-
-Si votre terminal est utilisé par plusieurs personnes et lorsqu'un même terminal dispose de plusieurs logiciels de navigation, nous ne pouvons pas nous assurer de manière certaine que les services destinés à votre terminal correspondent bien à votre propre utilisation de ce terminal et non à celle d'un autre utilisateur de ce terminal. Le cas échéant, le partage avec d'autres personnes de l'utilisation de votre terminal et la configuration des paramètres de votre navigateur à l'égard des cookies, relèvent de votre libre choix et de votre responsabilité.
+Si votre terminal est utilisé par plusieurs personnes et lorsqu'un même terminal
+dispose de plusieurs logiciels de navigation, nous ne pouvons pas nous assurer
+de manière certaine que les services destinés à votre terminal correspondent bien
+à votre propre utilisation de ce terminal et non à celle d'un autre utilisateur
+de ce terminal. Le cas échéant, le partage avec d'autres personnes de
+l'utilisation de votre terminal et la configuration des paramètres de votre
+navigateur à l'égard des cookies, relèvent de votre libre choix et
+de votre responsabilité.
 `;
