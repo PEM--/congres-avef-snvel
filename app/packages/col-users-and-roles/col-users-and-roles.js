79b0f7ea1185b414f5e714ac2d62d727959f7b75
PEM-- <pemarchandet@gmail.com>
2015-09-29 10:16:14 +0200

    Users and roles - pending

---
+++ app/packages/col-users-and-roles/col-users-and-roles.js
@@ -0,0 +1,31 @@
+// Options used on the server and the client
+// const sharedOptions = {
+//   name: 'Dictionary',
+//   schema: {
+//     title: { type: String, label: 'Titre', min: 5, max: 256 },
+//     shortTitle: { type: String, label: 'Titre court', min: 5, max: 32 },
+//     msTileColor: { type: String, label: 'Couleur des tuiles Microsoft', min: 7, max: 7}
+//   },
+//   // Available subscriptions and publications
+//   subs: {
+//     All: {}
+//   }
+// };
+//
+// // Client only
+// if (Meteor.isClient) {
+//   class Dictionary extends SD.Structure.BaseCollection {}
+//   // Export instance
+//   SD.Structure.dictionary = new Dictionary(sharedOptions);
+// }
+//
+// // Server only
+// if (Meteor.isServer) {
+//   // Options used only on the server
+//   const serverOptions = {
+//     defaults: [Meteor.settings.public.dictionary]
+//   };
+//   class Dictionary extends SD.Structure.ServerBaseCollection {}
+//   // Export instance
+//   SD.Structure.dictionary = new Dictionary(sharedOptions, serverOptions);
+// }
---
+++ app/packages/col-users-and-roles/package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'pierreeric:col-users-and-roles',
+  version: '0.0.1'
+});
+
+Package.onUse(function(api) {
+  // Meteor's API version
+  api.versionsFrom('1.2');
+  // Dependencies of this package
+  // Dependencies for server and client
+  const shared = ['alanning:roles@1.2.13'];
+  api.use(shared.concat(['spacedrop:basecollection']));
+  api.imply(shared);
+  // Included files in this packages
+  // Files for server and client
+  api.addFiles([
+    'col-users-and-roles.js',
+  ]);
+});
