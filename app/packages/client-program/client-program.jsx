581cc56228a65aabab3c71e267564645d53c444a
PEM-- <pemarchandet@gmail.com>
2015-10-02 23:55:51 +0200

    Program search - pending

--- app/packages/client-program/client-program.jsx
+++ app/packages/client-program/client-program.jsx
@@ -1,10 +1,29 @@
-// Landing-page
+// Programs selection page
 
 // Create a logger
 const log = Logger.createLogger('Client Program');
 
 // Namespace flatteinng
 const { Component } = React;
+
+class Selector extends Component {
+  render() {
+    const { type } = this.props;
+    return (
+      <select name={type} className={`ui fluid search dropdown ${type}`} style={{opacity: 0}}>
+        <option value=''>Skills</option>
+        <option value='angular'>Angular</option>
+        <option value='css'>CSS</option>
+        <option value='design'>Graphic Design</option>
+        <option value='ember'>Ember</option>
+        <option value='html'>HTML</option>
+      </select>
+    );
+  }
+  componentDidMount() {
+    $('.dropdown').velocity({opacity: 1}).dropdown();
+  }
+}
 
 // Program page component
 class Program extends Component {
@@ -18,6 +37,7 @@ class Program extends Component {
               <section className='row'>
                 <div className='sixteen wide column'>
                   <h1>Programme</h1>
+                  <Selector type='program' />
                 </div>
               </section>
             </div>
