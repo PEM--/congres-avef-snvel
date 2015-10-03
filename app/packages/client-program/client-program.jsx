38e08744c30e67784caade1e119f81c10911336d
PEM-- <pemarchandet@gmail.com>
2015-10-03 21:11:19 +0200

    Program selection - pending

--- app/packages/client-program/client-program.jsx
+++ app/packages/client-program/client-program.jsx
@@ -15,12 +15,12 @@ class Selector extends Component {
   }
   render() {
     const { value, type, unselectedValue, options } = this.props;
+    log.debug('Rendering Selector', type, value);
     const nodes = options.map(
       (option) => <option
         key={option} value={option}>{option}
       </option>
     );
-    console.log('Rendering Selector', type, value);
     return (
       <select value={value} className={`ui fluid search dropdown ${type}`} style={{opacity: 0}}>
         <option value=''>{unselectedValue}</option>
@@ -31,13 +31,13 @@ class Selector extends Component {
   componentDidMount() {
     $(`.dropdown.${this.props.type}`).velocity({opacity: 1}).dropdown({
       onChange: (value, text, $choice) => {
-        console.log('Received', value, text, $choice, this.props);
+        log.debug('Received', value, text, $choice, this.props);
         this.props.handleChange(this.props.type, value);
       }
     });
   }
   componentWillReceiveProps(nextProps) {
-    console.log('Received props change', this.props.type, this.props.value, nextProps);
+    log.debug('Received props change', this.props.type, this.props.value, nextProps);
     // Force reset when parent changes the props
     if (nextProps.value === '') {
       $(`.dropdown.${this.props.type}`).dropdown('restore defaults');
@@ -45,60 +45,117 @@ class Selector extends Component {
   }
 }
 
-
-
-
 // Program page component
 class Program extends BaseReactMeteor {
   constructor(props) {
     super(props);
-    this.state = {showDaySelector: false};
-    this.handleSelectedProgram = (e) => {
-      log.debug('Program selected');
-      this.setState({showDaySelector: true});
-    };
-    this.render = () => {
-      log.debug('Rendering Program');
-      const { loading, items, programsSelector } = this.data;
-      if (loading) {
-        return this.loadingRenderer();
+    this.state = { selectedProgram: '', selectedDay: '', selectedHour: '' };
+    this.handleChange = (type, value) => {
+      log.debug('Received child change', type, value);
+      switch (type) {
+      case 'program':
+        //delete this.props.day;
+        //delete this.props.hour;
+        this.setState({selectedProgram: value, selectedDay: '', selectedHour: ''});
+        FlowRouter.setQueryParams({program: value, day: null, hour: null});
+        break;
+      case 'day':
+        //delete this.props.hour;
+        this.setState({selectedDay: value, selectedHour: ''});
+        FlowRouter.setQueryParams({day: value, hour: null});
+        break;
+      case 'hour':
+        this.setState({selectedHour: value});
+        FlowRouter.setQueryParams({hour: value});
+        break;
+      default:
+        console.warn('Unknown type', type);
       }
-      console.warn('*** Programs', programsSelector);
-      return (
-        <div className='client main-content ui grid program'>
-          <div className='row'>
-            <div className='sixteen wide column'>
-              <div className='ui grid container'>
-                <section className='row'>
-                  <div className='sixteen wide column'>
-                    <h1>Programme</h1>
-                    <Selector
-                      key='program'
-                      type='program'
-                      unselectedValue='Programme'
-                      options={programsSelector}
-                      selectCb={this.handleSelectedProgram}
-                    />
-                    {
-                      this.state.showDaySelector ?
-                        <p>Select day</p> : ''
-                    }
-                  </div>
-                </section>
-                <SocialSharers />
-              </div>
-            </div>
-          </div>
-        </div>
-      );
     };
   }
   getMeteorData() {
     const handle = programs.subAll();
     const loading = !handle.ready();
     const items = loading ? [] : programs.collection.find().fetch();
-    const programsSelector = loading ? [] : _.uniq(_.flatten(_.pluck(items, 'programs')));
-    return { loading, items, programsSelector };
+    return { loading, items };
+  }
+  _setParams() {
+    const { items } = this.data;
+    this.programsOptions = _.uniq(_.flatten(_.pluck(items, 'programs')));
+    const { program, day, hour } = this.props;
+    if (program && this.programsOptions.find((item) => program === item)) {
+      this.state.selectedProgram = program;
+    }
+    if (this.state.selectedProgram !== '') {
+      const reducedItemsOnProgram = _.filter(items,
+        (item) => _.contains(item.programs, this.state.selectedProgram));
+      this.daysOptions = _.uniq(_.pluck(reducedItemsOnProgram, 'day'));
+      if (day && this.daysOptions.find((item) => day === item)) {
+        this.state.selectedDay = day;
+      }
+      if (this.state.selectedDay) {
+        const reducedItemsOnDays = _.where(reducedItemsOnProgram, {day: this.state.selectedDay});
+        this.hoursOptions = [];
+      }
+    }
+  }
+  render() {
+    log.debug('Rendering Program');
+    const { loading, items } = this.data;
+    if (loading) {
+      return this.loadingRenderer();
+    }
+    this._setParams();
+    const { selectedProgram, selectedDay, selectedHour } = this.state;
+    return (
+      <div className='client main-content ui grid program'>
+        <div className='row'>
+          <div className='sixteen wide column'>
+            <div className='ui grid container'>
+              <section className='row'>
+                <div className='sixteen wide column'>
+                  <Selector
+                    type='program'
+                    value={selectedProgram}
+                    unselectedValue='Sélectionner un programme'
+                    options={this.programsOptions}
+                    handleChange={this.handleChange}
+                  />
+                  {
+                    selectedProgram === '' ? '' :
+                      <Selector
+                        type= 'day'
+                        value={selectedDay}
+                        unselectedValue='Sélectionner un jour'
+                        options={this.daysOptions}
+                        handleChange={this.handleChange}
+                      />
+                  }
+                  {
+                    selectedDay === '' ? '' :
+                      <Selector
+                        type='hour'
+                        value={selectedHour}
+                        unselectedValue='Sélectionner un horraire'
+                        options={this.hoursOptions}
+                        handleChange={this.handleChange}
+                      />
+                  }
+                  {
+                    selectedHour === '' ? '' :
+                    <p>Available program</p>
+                  }
+                </div>
+              </section>
+              {
+                selectedHour === '' ? '' :
+                <SocialSharers />
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
@@ -106,9 +163,13 @@ class Program extends BaseReactMeteor {
 const ROUTE_NAME = 'program';
 FlowRouter.route(`/${ROUTE_NAME}`, {
   name: ROUTE_NAME,
-  action() {
+  action(params, queryParams) {
     ReactLayout.render(MainLayout, {
-      content: <Program />
+      content: <Program
+          program={queryParams.program}
+          day={queryParams.day}
+          hour={queryParams.hour}
+        />
     });
   }
 });
