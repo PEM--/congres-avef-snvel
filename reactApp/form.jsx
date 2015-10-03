9778d4061ce9e067832c8414cb4b9075f7b00e54
PEM-- <pemarchandet@gmail.com>
2015-10-03 13:04:09 +0200

    Form integration - pending

--- reactApp/form.jsx
+++ reactApp/form.jsx
@@ -1,8 +1,48 @@
-class Form extends React.Component {
+const { Component } = React;
+
+class Selector extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const { unselectedValue, options } = this.props;
+    const nodes = options.map(
+      (option) => <option
+        key={option} value={option}>{option}
+      </option>
+    );
     return (
-      <form>
-        <p>An empty form</p>
+      <select value=''>
+        <option value=''>{unselectedValue}</option>
+        {nodes}
+      </select>
+    );
+  }
+}
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedProgram: ''
+    };
+  }
+  onProgramChange(e) {
+    console.log('Change', e.target.value);
+    this.setState({selectedProgram: e.target.value});
+  }
+  render() {
+    const options = ['AVEF', 'SNVEL', 'EBMS'];
+    return (
+      <form onChange={this.onProgramChange} className='ui form'>
+        <Selector
+          unselectedValue='Programme'
+          options={options}
+        />
+        {
+          this.state.selectedProgram === '' ? '' :
+            <p>Program selected: {this.state.selectedProgram}</p>
+        }
       </form>
     );
   }
