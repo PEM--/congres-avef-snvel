75faa3d95f79c8ebfbe489fc2c42d8733516278d
PEM-- <pemarchandet@gmail.com>
2015-10-03 13:28:03 +0200

    Form integration - pending

--- reactApp/form.jsx
+++ reactApp/form.jsx
@@ -5,14 +5,14 @@ class Selector extends Component {
     super(props);
   }
   render() {
-    const { unselectedValue, options } = this.props;
+    const { value, type, unselectedValue, options } = this.props;
     const nodes = options.map(
       (option) => <option
         key={option} value={option}>{option}
       </option>
     );
     return (
-      <select value=''>
+      <select value={value} className={`ui dropdown ${type}`}>
         <option value=''>{unselectedValue}</option>
         {nodes}
       </select>
@@ -24,24 +24,46 @@ class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedProgram: ''
+      selectedProgram: '',
+      selectedDay: ''
+    };
+    this.onProgramChange = (e) => {
+      console.log('Change', e.target.value, e.target);
+      const $target = $(e.target);
+      if ($target.hasClass('program')) {
+        console.log('Changing program must invalidate day and hours');
+        this.setState({
+          selectedProgram: e.target.value,
+          selectedDay: ''
+        });
+      } else if ($target.hasClass('day')) {
+        console.log('Changing day must invalidate hours');
+        this.setState({
+          selectedDay: e.target.value
+        });
+      }
     };
   }
-  onProgramChange(e) {
-    console.log('Change', e.target.value);
-    this.setState({selectedProgram: e.target.value});
-  }
   render() {
-    const options = ['AVEF', 'SNVEL', 'EBMS'];
+    const programs = ['AVEF', 'SNVEL', 'EBMS'];
+    const days = ['Mardi', 'Mercredi', 'Jeudi'];
+    const { selectedProgram, selectedDay } = this.state;
     return (
       <form onChange={this.onProgramChange} className='ui form'>
         <Selector
-          unselectedValue='Programme'
-          options={options}
+          type='program'
+          value={selectedProgram}
+          unselectedValue='Sélectionner un programme'
+          options={programs}
         />
         {
-          this.state.selectedProgram === '' ? '' :
-            <p>Program selected: {this.state.selectedProgram}</p>
+          selectedProgram === '' ? '' :
+            <Selector
+              type= 'day'
+              value={selectedDay}
+              unselectedValue='Sélectionner un jour'
+              options={days}
+            />
         }
       </form>
     );
