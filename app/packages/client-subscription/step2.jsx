2cf367d8dab966c99544e508432b5eba23aa0a25
PEM-- <pemarchandet@gmail.com>
2015-10-06 00:46:23 +0200

    Subscription - Step 2

--- app/packages/client-subscription/step2.jsx
+++ app/packages/client-subscription/step2.jsx
@@ -3,7 +3,19 @@ const { Component } = React;
 
 class SubscriptionStep2 extends Component {
   render() {
-    return <p>Step 2</p>;
+    return (
+      <div className='ui icon message step2'>
+        <i className='fa fa-hourglass-half fa-4x'></i>
+        <div className='content'>
+          <h2 className='header'>Validation de votre email</h2>
+          <p>Un email de vérification vous a été transmis.</p>
+          <p>
+            Une fois reçu, veuillez cliquer le lien contenu dans ce dernier
+            pour passer à la prochaine étape de votre inscription.
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
