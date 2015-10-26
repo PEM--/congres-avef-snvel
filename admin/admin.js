ba4de4b688f4f6072b4cd785737b76c1e731704a
PEM-- <pemarchandet@gmail.com>
2015-10-26 11:06:46 +0100

    Internal admin login

--- admin/admin.js
+++ admin/admin.js
@@ -1,5 +1,24 @@
+AccountsTemplates.configure({
+  confirmPassword: false,
+  enablePasswordChange: false,
+  forbidClientAccountCreation: true,
+  sendVerificationEmail: false,
+  homeRoutePath: '/admin',
+  redirectTimeout: 2000,
+});
+
 AdminConfig = {
+  name: 'My App',
   adminEmails: ['pemarchandet@gmail.com'],
   collections: {
   }
 };
+
+if (Meteor.isClient) {
+  Accounts.onLogin(() => FlowRouter.go('/admin'));
+  FlowRouter.route('/', {
+    action() {
+      BlazeLayout.render('login');
+    }
+  });
+}
