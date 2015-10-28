5e18f2f53debe6f22c2ac9d1dd8b49e191fe02fb
PEM-- <pemarchandet@gmail.com>
2015-10-28 15:42:10 +0100

    Tabular begins

--- admin/lib/SharedTables.js
+++ admin/lib/SharedTables.js
@@ -1,19 +1,25 @@
-SharedTables = {};
-
-SharedTables.Users = new Tabular.Table({
-  name: 'Users',
-  collection: Meteor.users,
-  columns: [
-    {data: 'createdAt', title: 'Crée le'}
-  ]
-});
-
-SharedTables.BasicPages = new Tabular.Table({
-  name: 'BasicPages',
-  collection: SD.Structure.basicPages.collection,
-  columns: [
-    {data: 'title', label: 'Titre'}
-  ]
-});
-
+SharedTables = {
+  Users: new Tabular.Table({
+    name: 'Users',
+    collection: Meteor.users,
+    columns: [{
+      data: 'emails', title: 'Email', render(val, type, doc) {
+        const user = Meteor.users.findOne(doc._id);
+        console.log('user', user, doc);
+        return user.emails[0].address;
+      }
+    }, {
+      data: 'createdAt', title: 'Crée le', render(val) {
+        return moment(val).format('DD/MM/YYYY HH:mm');
+      }
+    }]
+  }),
+  BasicPages: new Tabular.Table({
+    name: 'BasicPages',
+    collection: SD.Structure.basicPages.collection,
+    columns: [
+      {data: 'title', label: 'Titre'}
+    ]
+  })
+};
 console.log('Tabular declared');
