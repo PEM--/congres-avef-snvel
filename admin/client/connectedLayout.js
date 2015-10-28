24a37b1bc6f09723b3cbd000ec4f61795e5683a3
PEM-- <pemarchandet@gmail.com>
2015-10-28 13:34:25 +0100

    Route protection

---
+++ admin/client/connectedLayout.js
@@ -0,0 +1,17 @@
+FlowRouter.route('/dashboard', {
+  name: 'dashboard',
+  triggersEnter: [
+    function(context, redirect) {
+      if (!Meteor.userId() ||
+        !Roles.userIsInRole(Meteor.userId(), 'admin')
+      ) {
+        console.warn('User', Meteor.userId(), 'Roles', Roles.userIsInRole(Meteor.userId(), 'admin'));
+        sAlert.error('Connexion requise');
+        FlowRouter.redirect('/');
+      }
+    }
+  ],
+  action() {
+    BlazeLayout.render('connectedLayout', {main: 'dashboard'});
+  }
+});
