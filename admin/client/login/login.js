0abad40134135aae11bbeb0036f140a857da6144
PEM-- <pemarchandet@gmail.com>
2015-10-28 12:14:47 +0100

    Login screen

--- admin/client/login/login.js
+++ admin/client/login/login.js
@@ -2,8 +2,20 @@ Template.login.helpers({
   mailto() {
     return `mailto:${s.escapeHTML('Pierre-Eric Marchandet ')}<pemarchandet@gmail.com>?` +
       `cc=` +
-        `${s.escapeHTML('Anne Daumas-Marchandet')} <adaumas@snvel.fr>,` +
-        `${s.escapeHTML('Claire Scicluna')} <plessisdoc@wanadoo.fr>` +
-      `subject=${s.escapeHTML('Congrès AVEF - SNVEL - Demande d\'accès administratif')}`;
+        `${s.escapeHTML('Anne Daumas-Marchandet ')}<adaumas@snvel.fr>,` +
+        `${s.escapeHTML('Claire Scicluna ')}<plessisdoc@wanadoo.fr>` +
+        '&' +
+      `subject=${s.escapeHTML("Congrès AVEF - SNVEL - Accès administratif")}`;
+  }
+});
+
+Template.login.events({
+  'submit form': function(e, t) {
+    // e.preventDefault();
+    console.log('Form', e);
+  },
+  'click button': function(e, t) {
+    // e.preventDefault();
+    console.log('Event', e);
   }
 });
--- admin/client/mainLayout.js
+++ admin/client/mainLayout.js
@@ -1,4 +1,13 @@
 FlowRouter.route('/', {
+  triggersEnter: [
+    function(context, redirect) {
+      if (Meteor.userId() &&
+        Roles.userIsInRole(Meteor.userId(), 'admin')
+      ) {
+        FlowRouter.redirect('dashboard');
+      }
+    }
+  ],
   action() {
     BlazeLayout.render('mainLayout', {main: 'login'});
   }
