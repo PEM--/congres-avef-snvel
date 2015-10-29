339554710a1c8715dbd4de9cc71d6299e2ddd7df
PEM-- <pemarchandet@gmail.com>
2015-10-29 09:01:00 +0100

    Autoform package bumped

--- admin/client/connectedLayout/settings/settings.js
+++ admin/client/connectedLayout/settings/settings.js
@@ -1,8 +1,15 @@
 Template.settings.onCreated(function() {
-  this.subscribe('DictionaryAll', {
+  this.subscribe('DictionaryAll'/*, {
     onReady: () => { this.data = SD.Structure.dictionary.collection.findOne(); }
-  });
+  }*/);
   console.log('currentTemplate', this);
+});
+
+Template.settings.helpers({
+  data() {
+    console.log('Data', SD.Structure.dictionary.collection.findOne());
+    return SD.Structure.dictionary.collection.findOne();
+  }
 });
 
 Template.settings.onRendered(function() {
