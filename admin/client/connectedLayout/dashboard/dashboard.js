1226305e6bf7963124b8b1d8faa63e66ee41c52e
PEM-- <pemarchandet@gmail.com>
2015-10-29 14:58:49 +0100

    Dashboard widgets

--- admin/client/connectedLayout/dashboard/dashboard.js
+++ admin/client/connectedLayout/dashboard/dashboard.js
@@ -1,3 +1,8 @@
+Template.dashboard.onCreated(function() {
+  this.subscribe('SubscribersAll');
+  this.subscribe('ProgramsAll');
+});
+
 Template.dashboard.onRendered(function() {
   $('.main-title').children().text('Dashboard');
 });
@@ -5,5 +10,20 @@ Template.dashboard.onRendered(function() {
 Template.dashboard.helpers({
   table() {
     return SharedTables.Users;
-  }
+  },
+  usersCount() {
+    const count = Meteor.users.find().count();
+    return numeral(count).format('0a');
+  },
+  usersLabel() { return 'Nombre d\'inscrits'; },
+  subscribersCount() {
+    const count = SD.Structure.subscribers.collection.find().count();
+    return numeral(count).format('0a');
+  },
+  subscribersLabel() { return 'Nombre d\'adhérents'; },
+  programsCount() {
+    const count = SD.Structure.programs.collection.find().count();
+    return numeral(count).format('0a');
+  },
+  programsLabel() { return 'Nombre de programmes'; }
 });
