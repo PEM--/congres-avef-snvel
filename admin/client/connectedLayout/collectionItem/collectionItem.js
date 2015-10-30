0d6f19ce87097825c1a461870f04951a10ce3bd7
PEM-- <pemarchandet@gmail.com>
2015-10-30 14:40:40 +0100

    Unknown document for collection

--- admin/client/connectedLayout/collectionItem/collectionItem.js
+++ admin/client/connectedLayout/collectionItem/collectionItem.js
@@ -12,16 +12,29 @@ Template.collectionItem.onCreated(function() {
     }
     const title = this.routeDef.conf.title;
     $('.main-title').children().text(title);
-    this.subscribe(`${currentRoute}All`);
+    this.subscribe('uniqueItem', currentDocument, currentRoute, {
+      onReady: () => {
+        console.log('Subscription done');
+        this.data = this.routeDef.conf.collection.findOne(currentDocument);
+        if (!this.data) {
+          console.warn('Document invalide', currentRoute, currentDocument);
+          sAlert.error('Document invalide');
+          return FlowRouter.go('/dashboard');
+        }
+      },
+      onStop: () => {
+        console.warn('Subscription stopped', currentRoute, currentDocument);
+        sAlert.error('Document invalide');
+        return FlowRouter.go('/dashboard');
+      }
+    });
   });
-
 });
-
-
-
 
 Template.collectionItem.helpers({
   dataAvailable() {
-    return false;
+    const instance = Template.instance();
+    console.log('Data available ?', instance);
+    return instance.routeDef && instance.data;
   }
 });
--- admin/server/publishs.js
+++ admin/server/publishs.js
@@ -1,3 +1,31 @@
+Meteor.publish('uniqueItem', function(documentId, collectionName) {
+  if (!this.userId || !Roles.userIsInRole(this.userId, 'admin')) {
+    throw new Meteor.Error('admin', 'Unauthorized');
+  }
+  try {
+    check(documentId, String);
+    check(collectionName, String);
+    const definition = _.find(SharedTablesDefinition, route => {
+      return route.name.toLowerCase() === collectionName;
+    });
+    if (!definition) {
+      throw new Meteor.Error('admin', 'Unknown collection');
+    }
+    const publication = definition.conf.collection.find({_id: documentId});
+    if (publication.count() === 0) {
+      throw new Meteor.Error('admin', 'Unknown document');
+    }
+    return definition.conf.collection.find({_id: documentId});
+  } catch (error) {
+    console.warn('Error publising document', error);
+    // Relaunch error done by this function
+    if (error.error === 'admin') {
+      throw error;
+    }
+    throw new Meteor.Error('admin', error.toString());
+  }
+});
+
 Meteor.publish('dasboardCollections', function() {
   if (!this.userId || !Roles.userIsInRole(this.userId, 'admin')) {
     throw new Meteor.Error('admin', 'Unauthorized');
