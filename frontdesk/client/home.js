240c6e1d681ba333fb5d06995f13a4dcc99ec639
PEM-- <pemarchandet@gmail.com>
2015-11-01 18:09:15 +0100

    Replace Ionic by Materialize

--- frontdesk/client/home.js
+++ frontdesk/client/home.js
@@ -1,26 +1,26 @@
-Template.home.events({
-  'click button.photo': function(e, t) {
-    $('body').css('background', 'yellow');
-    MeteorCamera.getPicture(function(error, data) {
-      if (error) {
-        return $('body').css('background', 'red');
-      }
-      $('body').css('background', 'green')
-        .append(`<img src=${data}>`);
-    });
-  },
-  'click button.scan': function(e, t) {
-    $('body').css('background', 'violet');
-    cordova.plugins.barcodeScanner.scan(
-      function(result) {
-        $('body').css('background', 'green')
-          .append(`<p>Result: ${result.text}</p>`)
-          .append(`<p>Format: ${result.format}</p>`)
-          .append(`<p>Cancelled: ${result.cancelled}</p>`);
-      },
-      function(error) {
-        return $('body').css('background', 'red');
-      }
-    );
-  }
-});
+// Template.home.events({
+//   'click button.photo': function(e, t) {
+//     $('body').css('background', 'yellow');
+//     MeteorCamera.getPicture(function(error, data) {
+//       if (error) {
+//         return $('body').css('background', 'red');
+//       }
+//       $('body').css('background', 'green')
+//         .append(`<img src=${data}>`);
+//     });
+//   },
+//   'click button.scan': function(e, t) {
+//     $('body').css('background', 'violet');
+//     cordova.plugins.barcodeScanner.scan(
+//       function(result) {
+//         $('body').css('background', 'green')
+//           .append(`<p>Result: ${result.text}</p>`)
+//           .append(`<p>Format: ${result.format}</p>`)
+//           .append(`<p>Cancelled: ${result.cancelled}</p>`);
+//       },
+//       function(error) {
+//         return $('body').css('background', 'red');
+//       }
+//     );
+//   }
+// });
---
+++ frontdesk/client/route.js
@@ -0,0 +1,23 @@
+Router.configure({
+  fastRender: true
+});
+
+// Create a global router controller
+MainController = RouteController.extend({
+  layoutTemplate: 'mainLayout',
+});
+
+// Main application (outside the logged routes)
+// Routes working on the MainController
+[
+  {url: '/frontdesk', options: {name: 'home'}}
+].forEach(rt => {
+  let obj = _.extend({controller: 'MainController'}, rt.options);
+  Router.route(rt.url, obj);
+});
+
+if (Meteor.isClient) {
+  Meteor.startup(function() {
+    Router.go('/frontdesk');
+  });
+}
