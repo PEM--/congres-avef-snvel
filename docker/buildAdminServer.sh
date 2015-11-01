4307abd0747b090a8f0ad2ba923fd5e758180f77
PEM-- <pemarchandet@gmail.com>
2015-11-01 12:12:01 +0100

    Build & clean scripts

--- docker/buildAdminServer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf adminserver/bundle
-cd ../admin
-meteor build --architecture os.linux.x86_64 --directory ../docker/adminserver
-cd -
--- docker/buildAppServer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf appserver/bundle nginx/bundle
-cd ../app
-meteor build --architecture os.linux.x86_64 --directory ../docker/appserver
-cd -
-cp -R appserver/bundle nginx
