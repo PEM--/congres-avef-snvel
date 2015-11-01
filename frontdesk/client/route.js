bd640975e7b195dd54208d32a4fcc9ad65679b4b
PEM-- <pemarchandet@gmail.com>
2015-11-01 19:39:39 +0100

    Main menu

--- frontdesk/client/route.js
+++ frontdesk/client/route.js
@@ -7,10 +7,11 @@ MainController = RouteController.extend({
   layoutTemplate: 'mainLayout',
 });
 
-// Main application (outside the logged routes)
 // Routes working on the MainController
 [
-  {url: '/frontdesk', options: {name: 'home'}}
+  {url: '/frontdesk', options: {name: 'home'}},
+  {url: '/frontdesk/session', options: {name: 'session'}},
+  {url: '/frontdesk/checkin', options: {name: 'checkin'}}
 ].forEach(rt => {
   let obj = _.extend({controller: 'MainController'}, rt.options);
   Router.route(rt.url, obj);
