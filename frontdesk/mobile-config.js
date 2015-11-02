b63bcf4707b09d42e677809f101ab2a5919477db
PEM-- <pemarchandet@gmail.com>
2015-11-02 15:55:55 +0100

    Circumventing indirect route for CORS protection on Android

--- frontdesk/mobile-config.js
+++ frontdesk/mobile-config.js
@@ -1,5 +1,11 @@
 App.info({
+  id: 'fr.pem-musing.blogspot.pierre-eric.marchandet',
   name: 'FrontDesk',
   description: 'FrontDesk for Congrès 2015',
-  version: '0.0.1'
+  version: '0.0.2',
+  author: 'Pierre-Eric Marchandet',
+  website: 'http://pem-musing.blogspot.fr/'
 });
+
+App.accessRule("*/congres-avef-snvel.fr/frontdesk/*");
+App.accessRule("*");
