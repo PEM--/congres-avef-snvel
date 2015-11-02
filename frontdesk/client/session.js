dddd74f4b5d4d20f1d5b7b72e3fab2da8fad6999
PEM-- <pemarchandet@gmail.com>
2015-11-02 04:40:57 +0100

    Selecting program

--- frontdesk/client/session.js
+++ frontdesk/client/session.js
@@ -1,6 +1,6 @@
 Session.setDefault('selectedDay', null);
 Session.setDefault('selectedHour', null);
-Session.setDefault('selectedProgram', null);
+Session.setDefault('selectedSession', null);
 
 Template.session.onDestroyed(function() {
   $('select').material_select('destroy');
@@ -12,17 +12,10 @@ Template.session.helpers({
   },
   dayValues() {
     const instance = Template.instance();
-    // const allSubsReady = Session.get('allSubsReady');
-    // if (allSubsReady) {
-    //   Meteor.setTimeout(() => {instance.$('select').material_select();}, 32);
-    //   const values = _.unique(_.pluck(SD.Structure.programs.collection.find().fetch(), 'day'));
-    //   console.log('Values', SD.Structure.programs.collection.find().fetch(), values);
-    //   return values;
-    // }
     Meteor.setTimeout(() => {instance.$('select.days').material_select();}, 32);
     const selectedDay = Session.get('selectedDay');
     values = [
-      {value: '', selected: selectedDay ? false : null, label: 'Sélectionner une journée'},
+      {value: '', selected: selectedDay === null ? true : false, label: 'Sélectionner une journée'},
       {value: 'Lundi', selected: selectedDay === 'Lundi', label: 'Lundi'},
       {value: 'Mardi', selected: selectedDay === 'Mardi', label: 'Mardi'},
       {value: 'Mercredi', selected: selectedDay === 'Mercredi', label: 'Mercredi'},
@@ -33,8 +26,52 @@ Template.session.helpers({
   isHourSelectable() {
     return Session.get('selectedDay') !== null;
   },
-  isProgramSelectable() {
+  hourValues() {
+    const instance = Template.instance();
+    const allSubsReady = Session.get('allSubsReady');
+    if (allSubsReady) {
+      Meteor.setTimeout(() => {instance.$('select.hours').material_select();}, 32);
+      const allHours = SD.Structure.programs.collection.find(
+        {day: Session.get('selectedDay')},
+        {sort: ['begin']}
+      ).fetch();
+      const selectedHour = Session.get('selectedHour');
+      let values = [
+        {value: '', selected: selectedHour === null ? true : false, label: 'Sélectionner un horraire'}
+      ];
+      allHours.forEach(hour => {
+        values.push(
+          {value: hour.begin, selected: selectedHour === hour.begin, label: `${hour.begin} - ${hour.end}`}
+        );
+      });
+      return values;
+    }
+    return [];
+  },
+  isSessionSelectable() {
     return Session.get('selectedHour') !== null;
+  },
+  sessionValues() {
+    const instance = Template.instance();
+    const allSubsReady = Session.get('allSubsReady');
+    if (allSubsReady) {
+      Meteor.setTimeout(() => {instance.$('select.sessions').material_select();}, 32);
+      const allSessions = SD.Structure.programs.collection.find(
+        {day: Session.get('selectedDay'), begin: Session.get('selectedHour')},
+        {sort: ['begin']}
+      ).fetch();
+      const selectedSession = Session.get('selectedSession');
+      let values = [
+        {value: '', selected: selectedSession === null ? true : false, label: 'Sélectionner un programme'}
+      ];
+      allSessions.forEach(session => {
+        values.push(
+          {value: session._id, selected: selectedSession === session._id, label: session.session}
+        );
+      });
+      return values;
+    }
+    return [];
   }
 });
 
@@ -42,16 +79,16 @@ Template.session.events({
   'change select.days': function(e, t) {
     Session.set('selectedDay', e.target.value === '' ? null : e.target.value);
     Session.set('selectedHour', null);
-    Session.set('selectedProgram', null);
+    Session.set('selectedSession', null);
   },
   'change select.hours': function(e, t) {
     Session.set('selectedHour', e.target.value === '' ? null : e.target.value);
-    Session.set('selectedProgram', null);
+    Session.set('selectedSession', null);
   },
-  'change select.programs': function(e, t) {
-    Session.set('selectedProgram', e.target.value === '' ? null : e.target.value);
+  'change select.sessions': function(e, t) {
+    Session.set('selectedSession', e.target.value === '' ? null : e.target.value);
     if (e.target.value !== '') {
-      Router.go('checkin');
+      Meteor.setTimeout(() => Router.go('checkin'), 64);
     }
   }
 });
