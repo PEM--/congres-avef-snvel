9ca6f9050272cfbbcdc4a3f819d2546e89a2a69c
PEM-- <pemarchandet@gmail.com>
2015-11-03 00:52:15 +0100

    Layout for badge preview

--- admin/client/connectedLayout/printing/printing.js
+++ admin/client/connectedLayout/printing/printing.js
@@ -1,15 +1,39 @@
 Template.printing.onCreated(function() {
   this.subscribe('dasboardCollections');
+  this.job = new ReactiveVar(null);
+  this.lastName = new ReactiveVar(null);
+  this.firstName = new ReactiveVar(null);
+  this.city = new ReactiveVar(null);
 });
 
 Template.printing.onRendered(function() {
   $('.main-title').children().text('Impression');
+  this.$('.qr-code-container').html(Meteor.user().profile.qrImage);
 });
 
 Template.printing.helpers({
   usersCount() {
     const count = Meteor.users.find().count();
     return numeral(count).format('0,0');
+  },
+  job() {
+    const instance = Template.instance();
+    return instance.job.get();
+  },
+  lastName() {
+    const instance = Template.instance();
+    return instance.lastName.get();
+  },
+  firstName() {
+    const instance = Template.instance();
+    return instance.firstName.get();
+  },
+  city() {
+    const instance = Template.instance();
+    return instance.city.get();
+  },
+  qrImage() {
+    return '<p><Hello</p>';
   }
 });
 
@@ -18,9 +42,16 @@ Template.printing.events({
     e.preventDefault();
     t.$('a.print').toggleClass('disabled');
     console.log('Printing');
-    let pdf = new PdfRenderer({size: 'a4'});
+    let pdf = new PdfRenderer({
+      size: 'a4',
+      margins: {
+        top: 570, bottom: 57,
+        left: 23, right: 23
+      }
+    });
     t.autorun(function() {
       if (pdf.ready()) {
+        // pdf.addPage();
         pdf.h1('Testing');
         pdf.finish('test.pdf', function() {
           t.$('a.print').toggleClass('disabled');
