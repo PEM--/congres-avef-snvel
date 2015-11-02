024dbdeeefaab4b6fb480e25bca7e025c58f1708
PEM-- <pemarchandet@gmail.com>
2015-11-02 06:27:57 +0100

    QR code scanning

--- frontdesk/client/checkin.js
+++ frontdesk/client/checkin.js
@@ -13,25 +13,28 @@ Template.checkin.helpers({
   }
 });
 
+const scanError = (error) => {
+  TTS.speak({
+    text: 'Une erreur est survenue pendant le scanne.',
+    locale: 'fr-FR'
+  }, function() {}, function() {});
+  sAlert.error('Erreur de scan: ' + error.toString());
+  console.warn('Error during scan' + error.toString());
+};
+
 Template.checkin.events({
-  'click button.speech': function(e, t) {
-    TTS.speak({
-      text: 'Bonjour Monsieur',
-      locale: 'fr-FR'
-    }, function() {}, function() {});
-  },
-  'click button.scan': function(e, t) {
-    $('body').css('background', 'violet');
+  'click a.scan': function(e, t) {
     cordova.plugins.barcodeScanner.scan(
       function(result) {
-        $('body').css('background', 'green')
-          .append(`<p>Result: ${result.text}</p>`)
-          .append(`<p>Format: ${result.format}</p>`)
-          .append(`<p>Cancelled: ${result.cancelled}</p>`);
+        if (result.cancelled) {
+          return scanError('Cancel');
+        }
+        if (result.format !== 'QR_CODE') {
+          return scanError('Not a QR code');
+        }
+        sAlert.success('Result: ' + result.toString());
       },
-      function(error) {
-        return $('body').css('background', 'red');
-      }
+      function(error) { scanError(error); }
     );
   }
 });
