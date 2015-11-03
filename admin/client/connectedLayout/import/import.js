3b9b437b2157648e33533b806dbda8a6df594e36
PEM-- <pemarchandet@gmail.com>
2015-11-03 13:10:55 +0100

    File uploader

--- admin/client/connectedLayout/import/import.js
+++ admin/client/connectedLayout/import/import.js
@@ -20,11 +20,48 @@ const treatFile = function(e, t) {
   t.pending.set(true);
   FS.Utility.eachFile(e, function(file) {
     const fsFile = new FS.File(file);
-    console.log('fsFile', fsFile);
     if (fsFile.type() !== 'text/csv') {
+      t.pending.set(false);
       return sAlert.error('Mauvais format');
     }
-    
+    let fr = new FileReader();
+    fr.onloadstart = function(frEvent) {
+      t.$('.file-upload')
+        .removeClass('grey')
+        .toggleClass('active green')
+        .progress({percent: 0});
+    };
+    fr.onprogress = function(frEvent) {
+      t.$('.file-upload').progress({percent: frEvent.loaded / frEvent.total});
+    };
+    fr.onload = function(frEvent) {
+      t.$('.progress.file-upload')
+        .progress({percent: 100});
+      t.$('.progress.user-insertion')
+        .removeClass('grey')
+        .progress({percent: 0})
+        .toggleClass('active blue');
+      const lines = frEvent.target.result.split('\n');
+      lines.forEach((line, idx) => {
+        Meteor.defer(() => {
+          console.log(idx, lines.length);
+          t.$('.progress.user-insertion').progress({percent: (idx + 1) / lines.length });
+          if (idx === lines.length - 1) {
+            sAlert.success('Insertion terminée');
+            t.$('.progress.file-upload')
+              .removeClass('active green')
+              .progress({percent: 0})
+              .addClass('grey');
+            // t.$('.progress.user-insertion')
+            //   .removeClass('active blue')
+            //   .progress({percent: 0})
+            //   .addClass('grey');
+            t.pending.set(false);
+          }
+        });
+      });
+    };
+    fr.readAsText(fsFile.data.blob, 'utf8');
   });
 };
 
